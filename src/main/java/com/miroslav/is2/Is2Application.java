3ac933e9e54aa26a0e9efbe5119bf398bb948b3e
Blacky001x <mmiroslav@uns.ac.rs>
2018-06-28 14:46:48 +0200

    Initial commit

---
+++ src/main/java/com/miroslav/is2/Is2Application.java
@@ -0,0 +1,12 @@
+package com.miroslav.is2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Is2Application.class, args);
+    }
+}
