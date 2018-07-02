5c207389187dae5db1eaf6ad322d40f3c020308e
Blacky001x <mmiroslav@uns.ac.rs>
2018-07-02 23:39:02 +0200

    quickfix

--- src/main/java/com/miroslav/is2/config/Config.java
+++ src/main/java/com/miroslav/is2/config/Config.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class Config {
 
-    @Bean
-    public JasperReportsViewResolver getJasperReportsViewResolver() {
-        JasperReportsViewResolver resolver = new JasperReportsViewResolver();
-        resolver.setPrefix("classpath:resources/jasperreports/");
-        resolver.setSuffix(".jasper");
-        resolver.setReportDataKey("datasource");
-        resolver.setViewNames("rpt_*");
-        resolver.setViewClass(JasperReportsMultiFormatView.class);
-        resolver.setOrder(0);
-        return resolver;
-    }
+//    @Bean
+//    public JasperReportsViewResolver getJasperReportsViewResolver() {
+//        JasperReportsViewResolver resolver = new JasperReportsViewResolver();
+//        resolver.setPrefix("classpath:resources/jasperreports/");
+//        resolver.setSuffix(".jasper");
+//        resolver.setReportDataKey("datasource");
+//        resolver.setViewNames("rpt_*");
+//        resolver.setViewClass(JasperReportsMultiFormatView.class);
+//        resolver.setOrder(0);
+//        return resolver;
+//    }
 
 }
